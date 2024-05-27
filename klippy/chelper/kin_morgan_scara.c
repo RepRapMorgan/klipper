ca84978b09dc7eb3a2a7f6b42923d35c7c28628b
Quentin Harley <quentin.harley@gmail.com>
2024-05-27 15:20:15 +0200

    Revert to cartesian logig in a new attempt

--- klippy/chelper/kin_morgan_scara.c
+++ klippy/chelper/kin_morgan_scara.c
@@ -1,85 +1,51 @@
-// Morgan SCARA kinematics stepper pulse time generation
+// scara kinematics stepper pulse time generation
 //
-// Copyright (C) 2019  Kevin O'Connor <kevin@koconnor.net>
-// Copyright (C) 2024  Quentin Harley <quentin.harley@gmail.com> (RepRapMorgan)
+// Copyright (C) 2018-2019  Kevin O'Connor <kevin@koconnor.net>
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include <math.h>   // sqrt, acos
-#include <stddef.h> // offsetof
 #include <stdlib.h> // malloc
-#include <stdio.h>
-#include <string.h>    // memset
-#include "compiler.h"  // __visible
+#include <string.h> // memset
+#include "compiler.h" // __visible
 #include "itersolve.h" // struct stepper_kinematics
-#include "trapq.h"     // move_get_coord
-//#define _USE_MATH_DEFINES
-//#include <cmath>
+#include "pyhelper.h" // errorf
+#include "trapq.h" // move_get_coord
 
-#define SQ(x) ((x) * (x))
-
-struct morgan_stepper
-{
-    struct stepper_kinematics sk;
-    double link_a, link_b;
-    double link_a2, link_b2;
-    double alpha, beta;
-};
-
-// returns the alpha angle in radians
 static double
-morgan_calc_alpha(struct stepper_kinematics *sk, struct move *m
+scara_stepper_x_calc_position(struct stepper_kinematics *sk, struct move *m
                              , double move_time)
 {
-    struct morgan_stepper *ms = container_of(sk, struct morgan_stepper, sk);
-    struct coord c = move_get_coord(m, move_time);
-    ms->beta = acos((SQ(c.x) + SQ(c.y) - ms->link_a2 - ms->link_b2)
-        / (2 * ms->link_a2 * ms->link_b2));
-    ms->alpha = atan((c.y / c.x) - atan((ms->link_b * sin(ms->beta))
-        / (ms->link_a + ms->link_b * cos(ms->beta))));
-
-    return ms->alpha;
+    return move_get_coord(m, move_time).x;
 }
 
-// returns the beta angle in radians
 static double
-morgan_calc_beta(struct stepper_kinematics *sk, struct move *m
+scara_stepper_y_calc_position(struct stepper_kinematics *sk, struct move *m
                              , double move_time)
 {
-    struct morgan_stepper *ms = container_of(sk, struct morgan_stepper, sk);
-    struct coord c = move_get_coord(m, move_time);
-    ms->beta = acos((SQ(c.x) + SQ(c.y) - ms->link_a2 - ms->link_b2)
-        / (2 * ms->link_a2 * ms->link_b2));
-    // return the beta value already calculated in morgan_calc_alpha()
-    return ms->beta;
+    return move_get_coord(m, move_time).y;
 }
 
-struct stepper_kinematics *__visible morgan_scara_stepper_alloc(
-    char stepper, double link_a, double link_b)
+static double
+scara_stepper_z_calc_position(struct stepper_kinematics *sk, struct move *m
+                             , double move_time)
 {
-    struct morgan_stepper *ms = malloc(sizeof(*ms));
-    memset(ms, 0, sizeof(*ms));
+    return move_get_coord(m, move_time).z;
+}
 
-    ms->link_a = link_a;
-    ms->link_b = link_b;
-    ms->link_a2 = SQ(link_a);
-    ms->link_b2 = SQ(link_b);
-
-    /* Ensure itersolve is called for all X and Y moves */
-    ms->sk.active_flags = AF_X | AF_Y;
-
-    /* Setup itersolve callback */
-    switch (stepper)
-    {
-    case 'a':
-        ms->sk.calc_position_cb = morgan_calc_alpha;
-        break;
-    case 'b':
-        ms->sk.calc_position_cb = morgan_calc_beta;
-        break;
-    default:
-        break;
+struct stepper_kinematics * __visible
+morgan_scara_stepper_alloc(char axis)
+{
+    struct stepper_kinematics *sk = malloc(sizeof(*sk));
+    memset(sk, 0, sizeof(*sk));
+    if (axis == 'x') {
+        sk->calc_position_cb = scara_stepper_x_calc_position;
+        sk->active_flags = AF_X;
+    } else if (axis == 'y') {
+        sk->calc_position_cb = scara_stepper_y_calc_position;
+        sk->active_flags = AF_Y;
+    } else if (axis == 'z') {
+        sk->calc_position_cb = scara_stepper_z_calc_position;
+        sk->active_flags = AF_Z;
     }
-
-    return &ms->sk;
+    return sk;
 }
--- klippy/kinematics/morgan_scara.py
+++ klippy/kinematics/morgan_scara.py
@@ -1,144 +1,134 @@
-# Code for handling the kinematics of polar robots
+# Code for handling the kinematics of cartesian robots
+# Incrementatl tests for SCARA kinematics
 #
-# Copyright (C) 2018-2021  Kevin O'Connor <kevin@koconnor.net>
+# Copyright (C) 2016-2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import math, logging
-import stepper, chelper
+import logging
+import stepper
+from . import idex_modes
 
 class MorganScaraKinematics:
     def __init__(self, toolhead, config):
-        # Setup morgan rails
-        stepper_configs = [config.getsection('stepper_' + s) for s in 'abz']
-
-        # Link A (proximal arm segment)
-        rail_a = stepper.PrinterStepper(stepper_configs[0],
-                                     units_in_radians = True)
-        # Link B (distal arm segment)
-        rail_b = stepper.PrinterStepper(stepper_configs[1],
-                                     units_in_radians = True)
-        # Elevator
-        rail_z = stepper.LookupMultiRail(stepper_configs[2])
-        self.steppers = [rail_a, rail_b] + rail_z.get_steppers()
-        self.rails = [rail_z]
-        
-        # Create two dummy cartesian kinematics for homing
-        #self.printer = config.get_printer()
-        #ffi_main, ffi_lib = chelper.get_ffi()
-        #self.cartesian_kinematics_L = ffi_main.gc(ffi_lib.cartesian_stepper_alloc('a'), ffi_lib.free)
-        #self.cartesian_kinematics_R = ffi_main.gc(ffi_lib.cartesian_stepper_alloc('b'), ffi_lib.free)
-
-        config.get_printer().register_event_handler(
-            "stepper_enable:motor_off", self._motor_off)
-
-        # Read config
-        max_velocity, max_accel = toolhead.get_max_velocity()
-        self.max_z_velocity = config.getfloat('max_z_velocity', max_velocity,
-                                              above=0., maxval=max_velocity)
-        self.link_a = config.getfloat('link_a_length', above=0.)
-        self.link_b = config.getfloat('link_b_length', above=0.)
-        # Setup rotary morgan calibration helper
-        
-        # Setup iterative solver
-        rail_a.setup_itersolve('morgan_scara_stepper_alloc',
-                              'a', self.link_a, self.link_b)
-        rail_b.setup_itersolve('morgan_scara_stepper_alloc',
-                              'b', self.link_a, self.link_b)
-        rail_z.setup_itersolve('cartesian_stepper_alloc', 'z')
-        
+        self.printer = config.get_printer()
+        # Setup axis rails
+        self.dual_carriage_axis = None
+        self.dual_carriage_rails = []
+        self.rails = [stepper.LookupMultiRail(config.getsection('stepper_' + n))
+                      for n in 'xyz']
+        for rail, axis in zip(self.rails, 'xyz'):
+            rail.setup_itersolve('cartesian_stepper_alloc', axis.encode())
+        ranges = [r.get_range() for r in self.rails]
+        self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.)
+        self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.)
+        self.dc_module = None
+        if config.has_section('dual_carriage'):
+            dc_config = config.getsection('dual_carriage')
+            dc_axis = dc_config.getchoice('axis', {'x': 'x', 'y': 'y'})
+            self.dual_carriage_axis = {'x': 0, 'y': 1}[dc_axis]
+            # setup second dual carriage rail
+            self.rails.append(stepper.LookupMultiRail(dc_config))
+            self.rails[3].setup_itersolve('cartesian_stepper_alloc',
+                                          dc_axis.encode())
+            dc_rail_0 = idex_modes.DualCarriagesRail(
+                    self.rails[self.dual_carriage_axis],
+                    axis=self.dual_carriage_axis, active=True)
+            dc_rail_1 = idex_modes.DualCarriagesRail(
+                    self.rails[3], axis=self.dual_carriage_axis, active=False)
+            self.dc_module = idex_modes.DualCarriages(
+                    dc_config, dc_rail_0, dc_rail_1,
+                    axis=self.dual_carriage_axis)
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
-
+        self.printer.register_event_handler("stepper_enable:motor_off",
+                                            self._motor_off)
         # Setup boundary checks
-        # self.need_home = True
-        self.need_home = False
-        
-        # Self Limit prevents collisions with arm hardware
-        self.limit_xy_magnitude = (
-            # Distances of less than 45mm can cause collition
-            config.getfloat('min_base_distance', 45., above=0.),
-            self.link_a + self.link_b - 1
-        )
-
-        logging.info('SCARA with proximal/distal linkages {:.1f}/{:.1f}mm'
-                     .format(self.link_a, self.link_b))
-        logging.info(
-            'SCARA with min/max (x,y) distances {:.1f}/{:.1f}mm'
-            .format(self.limit_xy_magnitude[0], self.limit_xy_magnitude[1]))
-        self.home_position = (0, self.limit_xy_magnitude[0], 0)
-        self.set_position(self.home_position, ())
-
+        max_velocity, max_accel = toolhead.get_max_velocity()
+        self.max_z_velocity = config.getfloat('max_z_velocity', max_velocity,
+                                              above=0., maxval=max_velocity)
+        self.max_z_accel = config.getfloat('max_z_accel', max_accel,
+                                           above=0., maxval=max_accel)
+        self.limits = [(1.0, -1.0)] * 3
     def get_steppers(self):
         return [s for rail in self.rails for s in rail.get_steppers()]
-
-    def calc_tag_position(self):
-        spos = [s.get_tag_position() for s in self.steppers]
-        a_angle, b_angle, z = spos
-        midpoint = (a_angle - b_angle) / 2.0
-        distance = (self.link_a * math.cos(midpoint)
-            - math.sqrt(self.link_b ** 2
-                        - (self.link_a ** 2) * (math.sin(midpoint) ** 2)))
-
-        x, y = (math.cos(a_angle - midpoint) * distance,
-        math.sin(a_angle - midpoint) * distance)
-        return [x, y, z]
-
+    def calc_position(self, stepper_positions):
+        rails = self.rails
+        if self.dc_module:
+            primary_rail = self.dc_module.get_primary_rail().get_rail()
+            rails = (rails[:self.dc_module.axis] +
+                     [primary_rail] + rails[self.dc_module.axis+1:])
+        return [stepper_positions[rail.get_name()] for rail in rails]
+    def update_limits(self, i, range):
+        l, h = self.limits[i]
+        # Only update limits if this axis was already homed,
+        # otherwise leave in un-homed state.
+        if l <= h:
+            self.limits[i] = range
     def set_position(self, newpos, homing_axes):
-        for s in self.steppers:
-            s.set_position(newpos)
-        if tuple(homing_axes) == (0, 1, 2):
-            self.need_home = False
-
-    def _motor_off(self, print_time):
-        self.need_home = True
-
+        for i, rail in enumerate(self.rails):
+            rail.set_position(newpos)
+        for axis in homing_axes:
+            if self.dc_module and axis == self.dc_module.axis:
+                rail = self.dc_module.get_primary_rail().get_rail()
+            else:
+                rail = self.rails[axis]
+            self.limits[axis] = rail.get_range()
+    def note_z_not_homed(self):
+        # Helper for Safe Z Home
+        self.limits[2] = (1.0, -1.0)
+    def home_axis(self, homing_state, axis, rail):
+        # Determine movement
+        position_min, position_max = rail.get_range()
+        hi = rail.get_homing_info()
+        homepos = [None, None, None, None]
+        homepos[axis] = hi.position_endstop
+        forcepos = list(homepos)
+        if hi.positive_dir:
+            forcepos[axis] -= 1.5 * (hi.position_endstop - position_min)
+        else:
+            forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
+        # Perform homing
+        homing_state.home_rails([rail], forcepos, homepos)
     def home(self, homing_state):
-        # no home troubleshooting
-        homing_state.set_axes([2])
-        forcepos = list(self.home_position)
-        forcepos[2] = -1.
-        homing_state.home_rails(self.rails, forcepos, self.home_position)
-        
-        #kinematics = [self.cartesian_kinematics_L, self.cartesian_kinematics_R, self.rails[2]]
-        #prev_sks    = [stepper.set_stepper_kinematics(kinematic)
-        #    for stepper, kinematic in zip(self.steppers, kinematics)]
-
-        # home
-        #rails = [self.rails[0], self.rails[1], self.rails[2]]
-        #homing_state.set_axes([0, 1, 2])
-        #forcepos = list(self.home_position)
-        #forcepos[2] = -1.
-        #homing_state.home_rails(rails, forcepos, self.home_position)
-
-        # swap back to original kinematics
-        #for stepper, prev_sk in zip(self.steppers, prev_sks):
-        #    stepper.set_stepper_kinematics(prev_sk)
-        # do kinematic math to figure out what x,y the home position actually is, and set it
-        #    self.toolhead.set_position( [x, y, 0, 0], (0, 1))
-        # All axes are homed simultaneously
-        
-
-    def check_move(self, move):
+        # Each axis is homed independently and in order
+        for axis in homing_state.get_axes():
+            if self.dc_module is not None and axis == self.dual_carriage_axis:
+                self.dc_module.home(homing_state)
+            else:
+                self.home_axis(homing_state, axis, self.rails[axis])
+    def _motor_off(self, print_time):
+        self.limits = [(1.0, -1.0)] * 3
+    def _check_endstops(self, move):
         end_pos = move.end_pos
-        if self.need_home:
-            raise move.move_error("Must home first")
-
-        distance = math.sqrt(end_pos[0] ** 2 + end_pos[1] ** 2)
-        if distance < self.limit_xy_magnitude[0]:
-            raise move.move_error(end_pos, "(x,y) pos too close to base joint")
-        elif distance >= self.limit_xy_magnitude[1]:
-            raise move.move_error(end_pos, "(x,y) pos exceeds reach of arms")
-        elif not move.axes_d[2]:
+        for i in (0, 1, 2):
+            if (move.axes_d[i]
+                and (end_pos[i] < self.limits[i][0]
+                     or end_pos[i] > self.limits[i][1])):
+                if self.limits[i][0] > self.limits[i][1]:
+                    raise move.move_error("Must home axis first")
+                raise move.move_error()
+    def check_move(self, move):
+        limits = self.limits
+        xpos, ypos = move.end_pos[:2]
+        if (xpos < limits[0][0] or xpos > limits[0][1]
+            or ypos < limits[1][0] or ypos > limits[1][1]):
+            self._check_endstops(move)
+        if not move.axes_d[2]:
+            # Normal XY move - use defaults
             return
+        # Move with Z - update velocity and accel for slower Z axis
+        self._check_endstops(move)
         z_ratio = move.move_d / abs(move.axes_d[2])
-        move.limit_speed(self.max_z_velocity * z_ratio,
-                         self.max_z_accel * z_ratio)
-
-    def get_status(self):
-        return {'homed_axes': '' if self.need_home else 'XYZ'}
-
-
+        move.limit_speed(
+            self.max_z_velocity * z_ratio, self.max_z_accel * z_ratio)
+    def get_status(self, eventtime):
+        axes = [a for a, (l, h) in zip("xyz", self.limits) if l <= h]
+        return {
+            'homed_axes': "".join(axes),
+            'axis_minimum': self.axes_min,
+            'axis_maximum': self.axes_max,
+        }
 
 def load_kinematics(toolhead, config):
     return MorganScaraKinematics(toolhead, config)
